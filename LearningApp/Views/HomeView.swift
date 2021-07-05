b05fea1d947e74a07616a5316a2bf97d6105ec28
Cm-pingouin <carlosmaloba4@gmail.com>
2021-07-05 14:38:39 +0200

    - build HomeView UI - build Model Card UI for HomeView in HomeViewRow

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    //Declaration proprety
+    @EnvironmentObject var model:ContentModel
+    
+    
     var body: some View {
-        Text("Hello, world! Welcome to the Home View")
-            .padding()
-    }
+        
+        NavigationView{
+            
+            VStack(alignment: .leading){
+                
+                Text("What do you want  to do today?")
+                    .padding(.leading,20)
+                
+                ScrollView{
+                    
+                    LazyVStack{
+                        
+                        ForEach(model.modules){ module in
+                            
+                            VStack(spacing:20){
+                                
+                                //Learning Card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                
+                                // Test Card
+                                HomeViewRow(image: module.test.image, title: "Test \(module.category)", description: module.test.description, count: "\(module.test.questions.count) Test", time: module.test.time)
+                            }
+                        }
+                    }.padding()
+                }
+            }
+            .navigationBarTitle("Get Started")
+        }
+    }  
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
+        
     }
 }
